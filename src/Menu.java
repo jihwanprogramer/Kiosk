037924b0fbd148a0229946b2ec1344bd354c09b7
https://github.com/jihwanprogramer/-3-.git <kob882333@naver.com>
2025-03-10 15:20:30 +0900

    feat(menu): implement MenuItem management for categories
    - Created MenuItem class to represent individual menu items
    - Added functionality to manage burger, drink, and dessert categories
    - Integrated MenuItem instances into respective category menus

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu {
+    List<MenuItem> menuItemList;
+    String category;
+
+    public Menu(String category) {
+        this.category = category;
+        this.menuItemList = new ArrayList<>();
+    }
+
+    public void addMenuItem(MenuItem item) {
+        menuItemList.add(item);
+    }
+
+    public List<MenuItem> getMenuItemList() {
+        return menuItemList;
+    }
+
+    public String getCategoryName() {
+        return category;
+    }
+
+    public void viewMenu() {
+        for (MenuItem menu : menuItemList) {
+            System.out.println((menuItemList.indexOf(menu) + 1) + ". " + menu.getName() + "|₩ " + menu.getPrice() + " |" + menu.getContent());
+        }
+        System.out.println("0. 뒤로가기");
+    }
 }
