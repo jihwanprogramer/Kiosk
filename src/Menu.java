d6ce38abc3a94b5a61373a8150c4cd6da6c0a021
https://github.com/jihwanprogramer/-3-.git <kob882333@naver.com>
2025-03-10 16:19:02 +0900

    refactor(menu): move menu list management to Menu class
    - Removed the menu list management from Kiosk class
    - Added functionality to manage menu items directly within Menu class
    - Updated Kiosk class to utilize Menu's internal management for better organization

--- src/Menu.java
+++ src/Menu.java
@@ -2,30 +2,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
-    List<MenuItem> menuItemList;
-    String category;
+    private final String categoryName;
+    private final List<MenuItem> menuItems;
 
-    public Menu(String category) {
-        this.category = category;
-        this.menuItemList = new ArrayList<>();
+    public Menu(String categoryName) {
+        this.categoryName = categoryName;
+        menuItems = new ArrayList<>();
     }
 
     public void addMenuItem(MenuItem item) {
-        menuItemList.add(item);
-    }
-
-    public List<MenuItem> getMenuItemList() {
-        return menuItemList;
-    }
-
-    public String getCategoryName() {
-        return category;
+        menuItems.add(item);
     }
 
     public void viewMenu() {
-        for (MenuItem menu : menuItemList) {
-            System.out.println((menuItemList.indexOf(menu) + 1) + ". " + menu.getName() + "|₩ " + menu.getPrice() + " |" + menu.getContent());
+        System.out.println("[ " + categoryName + " MENU ]");
+        for (int i = 0; i < menuItems.size(); i++) {
+            MenuItem item = menuItems.get(i);
+            System.out.println(i + 1+". "+item.getName() + " | ₩ "+item.getPrice()+ " | "+ item.getContent());
         }
         System.out.println("0. 뒤로가기");
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
     }
 }
