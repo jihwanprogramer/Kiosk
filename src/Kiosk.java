db6f74bf9cd8cd6b57eed799a40a4e447c2a434d
https://github.com/jihwanprogramer/-3-.git <kob882333@naver.com>
2025-03-10 16:19:52 +0900

    refactor(kiosk): remove menu list from Kiosk and retrieve data from Menu
    - Eliminated direct management of menu list in Kiosk class
    - Implemented methods in Menu class to retrieve menu items
    - Updated Kiosk to use Menu's methods for accessing menu data

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,63 +1,52 @@
-import java.util.Scanner;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Kiosk {
-    List<Menu> mainMenu;
+    private final List<Menu> mainMenu;
 
     public Kiosk() {
         mainMenu = new ArrayList<>();
     }
 
+    // Kiosk 에 메뉴 추가 메소드
+    public void addMenu(Menu menu) {
+        mainMenu.add(menu);
+    }
+
     public void start() {
         Scanner sc = new Scanner(System.in);
-
-        Menu burgerMenu = new Menu("BURGERS");
-        Menu drinkMenu = new Menu("DRINKING");
-        Menu dessertMenu = new Menu("DESSERT");
-
-        burgerMenu.addMenuItem(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        burgerMenu.addMenuItem(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        burgerMenu.addMenuItem(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        burgerMenu.addMenuItem(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
-        mainMenu.add(burgerMenu);
-
-        drinkMenu.addMenuItem(new MenuItem("cider", 1.0, "사이다"));
-        drinkMenu.addMenuItem(new MenuItem("vanilla shake", 2.0, "바닐라 쉐이크"));
-        drinkMenu.addMenuItem(new MenuItem("cola", 1.0, "콜라"));
-        mainMenu.add(drinkMenu);
-
-        dessertMenu.addMenuItem(new MenuItem("Potato Fries", 1.5, "감자튀김"));
-        dessertMenu.addMenuItem(new MenuItem("Shake Shake Chicken", 3.0, "쉑쉑치킨"));
-        mainMenu.add(dessertMenu);
-
         int categoryChoice;
 
         do {
             System.out.println("[ MAIN MENU ]");
-            for (Menu m : mainMenu) {
-                System.out.println((mainMenu.indexOf(m) + 1) + ". " + m.getCategoryName());
+            for (int i = 0; i < mainMenu.size(); i++) {
+                System.out.println((i + 1) + ". " + mainMenu.get(i).getCategoryName());
             }
             System.out.println("0. 종료");
             categoryChoice = sc.nextInt();
 
             if (categoryChoice > 0 && categoryChoice <= mainMenu.size()) {
-                Menu selectMenu = mainMenu.get(categoryChoice - 1);
-                System.out.println("[ " + selectMenu.getCategoryName() + " MENU ]");
-                selectMenu.viewMenu();
+                Menu selectedMenu = mainMenu.get(categoryChoice - 1);
+                selectedMenu.viewMenu();
+
                 System.out.println("메뉴를 선택하세요");
                 int itemChoice = sc.nextInt();
-                if (itemChoice == 0) {
-                    continue;
+
+                if (itemChoice > 0 && itemChoice <= selectedMenu.getMenuItems().size()) {
+                    MenuItem chosenItem = selectedMenu.getMenuItems().get(itemChoice - 1);
+                    System.out.println("선택한 메뉴:"+chosenItem.getName() + " | ₩ "+chosenItem.getPrice()+ " | "+ chosenItem.getContent());
+                } else if(itemChoice == 0) {
+                     continue;
                 }
-                MenuItem choiceItem = selectMenu.getMenuItemList().get(itemChoice - 1);
-                System.out.println(" 선택한 메뉴:" + choiceItem.getName() + "|₩ " + choiceItem.getPrice() + " |" + choiceItem.getContent());
+                else{
+                        System.out.println("올바르지 않은 선택입니다.");
+                }
+
+
             }
-        }
-        while (categoryChoice != 0);
+        } while (categoryChoice != 0);
 
         sc.close();
     }
-
-
 }
