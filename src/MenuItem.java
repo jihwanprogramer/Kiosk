a7a17514c730257e853ac29ceb55f615494108e4
https://github.com/jihwanprogramer/-3-.git <kob882333@naver.com>
2025-03-12 19:57:54 +0900

    docs(MenuItem): add comments
    - Added comments to clarify the functionality of the MenuItem class

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -3,20 +3,24 @@ public class MenuItem {
     private final Double price;
     private final String content;
 
+    //MenuItem 생성자
     public MenuItem(String name, Double price, String content) {
         this.name = name;
         this.price = price;
         this.content = content;
     }
 
+    //name 값 추출
     public String getName() {
         return name;
     }
 
+    //price 값 추출
     public Double getPrice() {
         return price;
     }
 
+    //content 값 추출
     public String getContent() {
         return content;
     }
