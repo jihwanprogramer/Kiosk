3814980697386068d142620ed0fc057f35a860f3
https://github.com/jihwanprogramer/-3-.git <kob882333@naver.com>
2025-03-14 10:49:05 +0900

    feat(Kiosk): add exception handling, comments, and user discount selection
    - Added exception handling for invalid menu and order choices.
    - Enhanced code readability by adding comments throughout the class.
    - Implemented user discount selection process to apply discounts based on user type.
    - Updated start() method to integrate user discount selection with the order process.

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -38,7 +38,6 @@ public class Kiosk {
             System.out.println("0. 종료");
 
 
-
             if (!cart.getCartList().isEmpty()) {
                 System.out.println("[ ORDER MENU ]");
                 if (orderMenu.isEmpty()) {
@@ -50,12 +49,16 @@ public class Kiosk {
             }
 
 
-
             for (Menu m : orderMenu) {
                 System.out.println((orderMenu.indexOf(m) + mainMenu.size() + 1) + ". " + m.getCategoryName());
             }
 
             categoryChoice = sc.nextInt();
+
+            if (categoryChoice == 0) {
+                System.out.println("키오스크를 종료합니다.");
+                break;
+            }
 
             if (categoryChoice > 0 && categoryChoice <= mainMenu.size()) {
                 Menu choiceMenu = mainMenu.get(categoryChoice - 1);
@@ -73,24 +76,28 @@ public class Kiosk {
                     if (cartChoice == 1) {
                         cart.addCartList(choseItem);
                         System.out.println(choseItem.getName() + " 이 장바구니에 추가되었습니다.");
-                    } else {
+                    } else if (cartChoice == 2) {
                         continue;
+                    } else {
+                        throw new IllegalArgumentException();
                     }
-                } else if (itemChoice == 0) {
-                    continue;
                 }
             } else if (categoryChoice > mainMenu.size() && categoryChoice <= orderMenu.size() + mainMenu.size()) {
                 cart.viewCart();
                 orderChoice = sc.nextInt();
-
                 if (orderChoice == 1) {
-                    System.out.println("주문 되었습니다. 금액은 ₩" + cart.getTotalPrice() + " 입니다");
+                    User.viewUser();
+                    int userChoice = sc.nextInt();
+                    System.out.printf("주문 되었습니다.할인률 %s %%를 적용하였고 최종 금액은 $%.1f 입니다%n", User.kioskViewSale(userChoice), User.lastAmount(userChoice));
                     cart.clearCart();
                 } else if (orderChoice == 2) {
-                    continue;
+                    break;
                 }
+            } else {
+                throw new IllegalArgumentException();
             }
-        } while (categoryChoice != 0);
+
+        } while (true);
 
         sc.close();
     }
